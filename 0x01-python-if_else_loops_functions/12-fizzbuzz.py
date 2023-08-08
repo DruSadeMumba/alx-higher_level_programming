a47257d16ae8b3be4595da3616b24353cb50f2f9
Dru Sade Mumba <drusademumba@gmail.com>
2023-08-08 08:11:24 -0700

    Fizzbuzz function

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@
 def fizzbuzz():
     for numbers in range(1, 101):
         if (numbers % 15 == 0):
-            print('{}'.format("FizzBuzz"), end="")
+            print('{}'.format("FizzBuzz"), end=" ")
         elif (numbers % 5 == 0):
             print('{}'.format("Buzz"), end=" ")
         elif (numbers % 3 == 0):
