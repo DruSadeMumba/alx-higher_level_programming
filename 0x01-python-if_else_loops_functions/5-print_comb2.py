3222f406122b9d31e2d311d3ce731f6e0878a42c
Dru Sade Mumba <drusademumba@gmail.com>
2023-08-08 04:24:37 -0700

    print numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
-for numbers in range(100):
-    if numbers < 10:
-        print(f'{0}{numbers:d}', end=", ")
-    elif numbers < 99:
-        print(f'{numbers:d}', end=", ")
-    else:
-        print('{}'.format(numbers))
+for numbers in range(10):
+    print(f'{0}{numbers:d}', end=", ")
+print(*range(10, 100), sep=", ")
