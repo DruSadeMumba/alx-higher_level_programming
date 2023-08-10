3cfb0fa00e5fe8c32d1552ee28c3b20863a446fb
Dru Sade Mumba <drusademumba@gmail.com>
2023-08-10 07:39:06 -0700

    Infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    x = len(sys.argv) - 1
+    result = 0
+    for i in range(x):
+        result += int(sys.argv[i + 1])
+    print("{}".format(result))
