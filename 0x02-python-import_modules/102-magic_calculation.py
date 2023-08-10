5e44a68bd26d386820ea173375a7e5e0b1ebf59d
Dru Sade Mumba <drusademumba@gmail.com>
2023-08-10 11:17:57 -0700

    correction

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,7 +4,7 @@ def magic_calculation(a, b):
     if a < b:
         c = a + b
         for i in range(4, 6):
-            c += i
+            c == c + i
         return c
     else:
         return (a - b)
