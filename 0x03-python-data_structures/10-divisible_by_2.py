1e7d1dda177069587378ad0083e6a3ce08a08a20
Dru Sade Mumba <drusademumba@gmail.com>
2023-08-13 10:48:23 -0700

    Write a function that finds all multiples of 2 in a list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    x = len(my_list)
+    new_list = []
+    for i in range(x):
+        if my_list[i] % 2 == 0:
+            new_list.append(True)
+        new_list.append(False)
+    return new_list
