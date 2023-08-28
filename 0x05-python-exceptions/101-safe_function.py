5523d0628b1901c3c72bfbd97bb6d9929f85976c
Dru Sade Mumba <drusademumba@gmail.com>
2023-08-28 06:58:29 -0700

    Write a function that executes a function safely

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+from sys import *
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except (IndexError, TypeError, ValueError, ZeroDivisionError) as err:
+        stderr.write('Exception: {}\n'.format(err))
