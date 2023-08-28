df3be00976b2c609b87d79751ef00e3b2443889d
Dru Sade Mumba <drusademumba@gmail.com>
2023-08-28 07:06:54 -0700

    Write the Python function def magic_calculation(a, b)

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+def magic_calculation(a, b):
+    x = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            x += (a**b) / i
+        except:
+            x = a + b
+            break
+    return x
