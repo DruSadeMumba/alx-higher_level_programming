9be995f6455fec48fae5323f8eb8d84b11bd9ee4
Dru Mumba <drusademumba@gmail.com>
2023-08-29 22:43:21 +0300

    Write a class Node that defines a node of a singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -15,9 +15,10 @@ class Node:
 
     @data.setter
     def data(self, value):
-        self.__data = value
         if type(value) is not int:
             raise TypeError("data must be an integer")
+        else:
+            self.__data = value
 
     @property
     def next(self):
@@ -25,9 +26,10 @@ class Node:
 
     @next.setter
     def next(self, value):
-        self.__next = value
         if type(value) is not Node and value is not None:
             raise TypeError("next_node must be a Node object")
+        else:
+            self.__next = value
 
 
 class SinglyLinkedList:
