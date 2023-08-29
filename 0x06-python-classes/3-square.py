2e56271a788c84019d4067a21fcb81fbf5a6e912
Dru Mumba <drusademumba@gmail.com>
2023-08-29 15:33:45 +0300

    write a class Square that defines a square safely and calculates the area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """square with private instance attribute: size"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be an integer")
+
+    def area(self):
+        area = self.__size ** 2
+        return area
