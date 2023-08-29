e60df8ccb6a6c922f386d9e9ad266b370030ca23
Dru Mumba <drusademumba@gmail.com>
2023-08-29 14:56:31 +0300

    write a class Square that defines a square safely

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""defines a square"""
+
+class Square:
+    """square with private size attribute"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
