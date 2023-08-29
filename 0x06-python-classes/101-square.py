0e4786c45b94f62e9f22156cb25934ba170ff8d9
Dru Mumba <drusademumba@gmail.com>
2023-08-29 21:07:33 +0300

    Write a class Square that defines a square

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,1 +1,54 @@
 #!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """square with private instance attribute: size"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        self.__position = value
+        if not isinstance(value, tuple) or len(value) != 2 \
+                or not all(isinstance(val, int) for val in value) \
+                or not all(val >= 0 for val in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        area = self.__size ** 2
+        return area
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            print("\n" * self.__position[1], end='')
+            for i in range(self.__size):
+                print("\n".join([" " * self.__position[0] +
+                                 "#" * self.__size]))
+
+    def __str__(self):
+        if self.__size == 0:
+            return ''
+        i = [" " * self.__position[0] + "#" * self.__size
+             for _ in range(self.__size)]
+        return "\n".join(["" * self.__position[1]] + i)
