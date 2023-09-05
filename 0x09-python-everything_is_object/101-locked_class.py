161061f39f2c2e9add1ff60a66e9d00d11694264
Dru Mumba <drusademumba@gmail.com>
2023-09-05 12:45:34 +0300

    Add Answers

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+"""Write a class LockedClass with no class or object attribute"""
+
+
 def LockedClass():
+    """new instance attribute is called first_name."""
     __slots__ = "first_name"
