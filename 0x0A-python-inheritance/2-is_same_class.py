1eff719a1f15f6f16a6dbd3d1f39e24ac5e4cfed
Dru Mumba <drusademumba@gmail.com>
2023-09-11 13:54:42 +0300

    Add function

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,6 +7,4 @@ exactly an instance of the specified class
 
 def is_same_class(obj, a_class):
     """are obj and a_class same"""
-    if type(obj) == a_class:
-        return True
-    return False
+    return type(obj) is a_class
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Write a function that returns True if the object is an instance of,
+or if the object is an instance of a class that inherited from,
+the specified class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """obj and a_class are same or instance of"""
+    return isinstance(obj, a_class)
