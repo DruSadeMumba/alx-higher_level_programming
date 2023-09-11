1d09730198f157d408b3799c1faba0f791cb3f80
Dru Mumba <drusademumba@gmail.com>
2023-09-11 14:06:15 +0300

    Add function

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""An empty class BaseGeometry"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""A class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class with various methods"""
+    def area(self):
+        """not defined"""
+        raise Exception("area() is not implemented")
