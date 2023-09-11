8693a3186a889467175cf0b42ed374bdcbaee749
Dru Mumba <drusademumba@gmail.com>
2023-09-11 15:42:10 +0300

    Add functions

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""a class MyInt that inherits from int"""
+
+
+class MyInt(int):
+    """rebel class"""
+
+    def __eq__(self, other):
+        return self.real != other
+
+    def __ne__(self, other):
+        return self.real == other
--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Add new attr to an obj"""
+
+
+def add_attribute(obj, att, val):
+    """add attr to obj"""
+    if hasattr(obj, '__dict__'):
+        setattr(obj, att, val)
+    else:
+        raise TypeError("can't add new attribute")
