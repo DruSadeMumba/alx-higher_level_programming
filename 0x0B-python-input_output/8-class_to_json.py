f196d954a5619f8a3239e4af6af7d2fb4e01d94e
Dru Mumba <drusademumba@gmail.com>
2023-09-12 14:46:03 +0300

    Add 'dict rep' function

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""
+Write a function that returns the dict desc with simple
+data struct for JSON serialization of an obj
+"""
+
+
+def class_to_json(obj):
+    """dict rep"""
+    return obj.__dict__
