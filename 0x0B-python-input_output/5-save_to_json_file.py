29a1c5b75420f10489e4f04a2281e8768960fdac
Dru Mumba <drusademumba@gmail.com>
2023-09-12 14:15:02 +0300

    Add 'create an Object from a JSON file' function

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """json to file"""
-    with open(filename, "w") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return json.dump(my_obj, f)
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Write a function that creates an Object from a JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """create obj"""
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
