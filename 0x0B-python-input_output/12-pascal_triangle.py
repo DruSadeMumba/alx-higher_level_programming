56e620dafe49d55591480fe8fa30cd2952c9a2fb
Dru Mumba <drusademumba@gmail.com>
2023-09-12 17:31:29 +0300

    Add pascal triangle function

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+Create a function that returns a list of lists
+of integers representing the Pascal’s triangle
+"""
+
+
+def pascal_triangle(n):
+    """pascal triangle"""
+    if n <= 0:
+        return []
+    tri = [[1]]
+    for i in range(n-1):
+        tri.append([a + b for a, b in
+                    zip([0] + tri[-1], tri[-1] + [0])])
+    return tri
