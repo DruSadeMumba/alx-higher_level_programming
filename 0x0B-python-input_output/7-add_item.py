429d8c5eb8a5ffc478145f67dc03c63121e6f62e
Dru Mumba <drusademumba@gmail.com>
2023-09-12 14:40:49 +0300

    Add 'script that adds all arguments to a Python list,' function

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+Write a script that adds all args to a PyList, and then save them to a file
+"""
+from sys import *
+Save = __import__('5-save_to_json_file').save_to_json_file
+Load = __import__('6-load_from_json_file').load_from_json_file
+
+
+if __name__ == "__main__":
+    try:
+        items = Load("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(argv[1:])
+    Save(items, "add_item.json")
