83eab9a3414178f0b3ff7b3249185f05e968869e
Dru Mumba <drusademumba@gmail.com>
2023-09-12 14:10:10 +0300

    Add Object to a text file function

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Write a function that writes an Obj to a txt file, using a JSON rep"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """json to file"""
+    with open(filename, "w") as f:
+        return json.dump(my_obj, f)
