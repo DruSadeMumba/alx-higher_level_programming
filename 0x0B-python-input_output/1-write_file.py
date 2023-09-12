b4aef69e243611ff1c677258b17aeaf28bf07e48
Dru Mumba <drusademumba@gmail.com>
2023-09-12 13:48:33 +0300

    Add write function

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Write a function that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """write to text file"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
