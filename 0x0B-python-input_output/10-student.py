bb300d923f47c417bbe2c50f6316eeb428db6b23
Dru Mumba <drusademumba@gmail.com>
2023-09-12 16:54:54 +0300

    Add 'student' class

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""defines a student"""
+
+
+class Student:
+    """a student"""
+    def __init__(self, first_name, last_name, age):
+        """init student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """dict rep"""
+        if attrs:
+            d = {}
+            for attr in attrs:
+                if attr in self.__dict__.keys():
+                    d[attr] = self.__dict__[attr]
+            return d
+        return self.__dict__
