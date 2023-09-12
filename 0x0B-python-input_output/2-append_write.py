a3e9a7990c5ce94fec451b94f97ff561f0e382cd
Dru Mumba <drusademumba@gmail.com>
2023-09-12 13:54:58 +0300

    Add append function

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Write a function that appends a string to a text file"""
+
+
+def append_write(filename="", text=""):
+    """add to text file"""
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
