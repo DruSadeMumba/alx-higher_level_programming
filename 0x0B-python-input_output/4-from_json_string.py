2c2034a7de86378959f6e4a66a7ac6db875c0e05
Dru Mumba <drusademumba@gmail.com>
2023-09-12 14:04:31 +0300

    Add from-json-str function

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Write a function that returns an object reped by a JSON str"""
+import json
+
+
+def from_json_string(my_str):
+    """from json-str"""
+    return json.loads(my_str)
