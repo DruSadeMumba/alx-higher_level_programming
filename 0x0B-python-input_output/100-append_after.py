00139e4a053f467a0325c637a3c6a2f9cad3075b
Dru Mumba <drusademumba@gmail.com>
2023-09-12 17:55:40 +0300

    Write a function that inserts a line of text to a file,

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""inserts a line of text to a file"""
+
+
+
+def append_after(filename="", search_string="", new_string=""):
+    "write in file"
+    text = ""
+    with open(filename, 'r+', encoding='utf-8') as f:
+        for line in f:
+            text += line
+            if search_string in line:
+                text += new_string
+        f.seek(0)
+        f.write(text)
