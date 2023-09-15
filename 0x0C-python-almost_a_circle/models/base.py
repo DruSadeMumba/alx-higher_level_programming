3c9959315c22e6899538e301ab19d7613a061e94
Dru Mumba <drusademumba@gmail.com>
2023-09-15 10:39:52 +0300

    Write the first class Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,4 +3,12 @@
 
 class Base:
     """Base class"""
-    pass
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """class constructor"""
+        if id:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
