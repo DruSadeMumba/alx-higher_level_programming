44fce0ba957402f968d18a6daef62c7b6e27b7bd
Dru Mumba <drusademumba@gmail.com>
2023-10-18 16:09:03 +0300

    Write a script that lists all shows contained in the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,1 +1,5 @@
 -- A script that lists all shows contained in the database hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
+
