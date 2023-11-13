d40482c0d333ce999156dca404494bec249e8eb4
Dru Mumba <drusademumba@gmail.com>
2023-11-13 15:02:29 +0300

    Write a script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+let count = process.argv.slice(2)[0];
+if (isNaN(count) || typeof count === 'undefined') {
+  console.log('Missing number of occurrences');
+} else {
+  count = parseInt(count, 10);
+  for (let i = 0; i < count; i++) {
+    console.log('X'.repeat(count));
+  }
+}
