13ab78c6b62eb666eb0ce9d7c4fe4069309680a4
Dru Mumba <drusademumba@gmail.com>
2023-11-18 22:50:04 +0300

    Write a script that creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -7,6 +7,6 @@ from relationship_state import Base
 class City(Base):
     """City class"""
     __tablename__ = 'cities'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
