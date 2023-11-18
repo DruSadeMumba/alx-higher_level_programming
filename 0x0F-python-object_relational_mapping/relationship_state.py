4b1abe4c42356bfcaecd5ab8bca1ad7542c2b60d
Dru Mumba <drusademumba@gmail.com>
2023-11-18 22:37:35 +0300

    Write a script that creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Class definition of a State and an instance Base"""
-from sqlalchemy import *
+from sqlalchemy import MetaData, Integer, Column, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
@@ -11,6 +10,5 @@ Base = declarative_base(metadata=mymetadata)
 class State(Base):
     """State class"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="states")
