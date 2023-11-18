164af7962ac8296a3340fd80fb6286a841011315
Dru Mumba <drusademumba@gmail.com>
2023-11-18 22:24:57 +0300

    Improve the files model_city.py and model_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -12,5 +12,5 @@ class State(Base):
     """State class"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
-    name = Column(String(128), nullable=False)
+    name = Column(String(128), nullable=False, unique=True)
     cities = relationship("City", backref="states")
