bf46771c10e2a35620b6de22964c9b06da8e36dd
Dru Mumba <drusademumba@gmail.com>
2023-11-18 21:51:52 +0300

    Improve the files model_city.py and model_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Class definition of a State and an instance Base"""
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    """State class"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="states")
