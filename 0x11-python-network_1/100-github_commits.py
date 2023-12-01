3ba45a9dfb81c0c6da4621105cba9d91ae06fdbe
Dru Mumba <drusademumba@gmail.com>
2023-12-01 22:30:14 +0300

    Print 10 GitHub commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,7 +4,7 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}"
+    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
     try:
         res = requests.get(url)
         commits = res.json()[:10]
