a6dbfe570f8d3e0c71ec1bc81a3cb26bef40af35
Dru Mumba <drusademumba@gmail.com>
2023-12-01 20:52:56 +0300

    Write a Python script that takes in a URL, sends a request to the URL and displays the body of the response.
    If the HTTP status code is greater than or equal to 400, print: Error code: followed by the value of the HTTP status code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""
+A python script that takes in a URL, sends a request
+to the URL and displays the body of the response
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        res = requests.get(url)
+        print(res.text)
+        if res.status_code >= 400:
+            print(f"Error code: {res.status_code}")
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
