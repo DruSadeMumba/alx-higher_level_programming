28b3b647360d6acdd8f7e1d46e9d7be06cf7fb06
Dru Mumba <drusademumba@gmail.com>
2023-12-01 12:20:25 +0300

    Write a Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+A python script that takes in a URL and an email, sends a POST request to the
+passed URL with the email as a parameter, and displays the body of the response
+"""
+import sys
+import urllib.request
+import urllib.parse
+import urllib.error
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email_data = urllib.parse.urlencode({"email": sys.argv[2]}).encode("utf-8")
+    try:
+        with urllib.request.urlopen(url, data=email_data) as res:
+            body_cont = res.read().decode("utf-8")
+            print(f"{body_cont}")
+    except urllib.error.URLError as e:
+        print("Error: ", e)
