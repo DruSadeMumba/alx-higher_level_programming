59466105d8708b1c2d2b92ad63fd4219d0e64976
Dru Mumba <drusademumba@gmail.com>
2023-12-01 22:17:46 +0300

    Print 10 GitHub commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}"
     try:
         res = requests.get(url)
-        commits = res.json()[:10]
-        for commit in commits:
-            sha = commit.get("sha")
-            name = commit.get("commit").get("author").get("name")
+        commits = res.json()
+        for commit in range(10):
+            sha = commits[commit].get("sha")
+            name = commits[commit].get("commit").get("author").get("name")
             print(f"{sha}: {name}")
     except requests.exceptions.RequestException as e:
         print("Error:", e)
