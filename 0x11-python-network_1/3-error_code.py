3d62ff760ea7229bb2452fbc5e1a26514a18a405
Dru Mumba <drusademumba@gmail.com>
2023-12-01 12:39:45 +0300

    Write a Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).
    You have to manage urllib.error.HTTPError exceptions and print: Error code: followed by the HTTP status code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+A python script that takes in a URL, sends a request
+to the URL and displays the body of the response
+"""
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as res:
+            body_cont = res.read().decode("utf-8")
+            print(f"{body_cont}")
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
+    except urllib.error.URLError as e:
+        print("Error: ", e)
