6391b2752e98069d08fdd59660fcf5817df9632a
Dru Mumba <drusademumba@gmail.com>
2023-12-01 22:13:43 +0300

    Print 10 GitHub commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Print 10 GitHub commits"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}"
+    try:
+        res = requests.get(url)
+        commits = res.json()[:10]
+        for commit in commits:
+            sha = commit.get("sha")
+            name = commit.get("commit").get("author").get("name")
+            print(f"{sha}: {name}")
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
