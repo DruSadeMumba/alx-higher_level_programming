5bb895bd79b6611754ff02aa22db387258a0ef0c
Dru Mumba <drusademumba@gmail.com>
2023-12-01 21:14:00 +0300

    Write a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
    The letter must be sent in the variable q
    If no argument is given, set q=""
    If the response body is properly JSON formatted and not empty, display the id and name like this: [<id>] <name>

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+A python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    try:
+        res = requests.post(url, data={"q": letter})
+        try:
+            result = res.json()
+            if result:
+                print(f"[{result.get('id')}] {result.get('name')}")
+            else:
+                print("No result")
+        except ValueError:
+            print("Not a valid JSON")
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
