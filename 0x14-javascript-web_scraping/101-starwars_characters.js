436a9134a302fe09e92036bd02a3de4f737aa65e
Dru Mumba <drusademumba@gmail.com>
2024-01-03 18:09:40 +0300

    Write a script that prints all characters of a Star Wars movie in the same order of the list “characters” in the /films/ response.

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,29 @@
-#!/usr/bin/env node
+#!/usr/bin/node
+const req = require('request');
+const id = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+const getChar = (url) => {
+  return new Promise((resolve, reject) => {
+    req(url, (error, response, charBody) => {
+      if (!error && response.statusCode === 200) {
+        const character = JSON.parse(charBody);
+        resolve(character.name);
+      } else {
+        reject(error || response.statusCode);
+      }
+    });
+  });
+}
+
+req(apiUrl, async (err, res, body) => {
+  if (!err && res.statusCode === 200) {
+    const data = JSON.parse(body);
+    const chars = await Promise.all(data.characters.map(getChar));
+    chars.forEach((charName) => {
+      console.log(charName);
+    });
+  } else {
+    console.error(err || res.statusCode);
+  }
+});
