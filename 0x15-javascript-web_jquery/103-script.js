284020067d8130295444eb30a5af3114e0323770
Dru Mumba <drusademumba@gmail.com>
2024-01-10 16:46:42 +0300

    Write a JavaScript script that fetches and prints how to say “Hello” depending on the language

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,1 +1,17 @@
-
+$(document).ready(() => {
+  const languageCodeInput = $('#language_code');
+  $('#btn_translate').click(() => {
+    translateHello();
+  });
+  languageCodeInput.keyup((event) => {
+    if (event.keyCode === 13) {
+      translateHello();
+    }
+  });
+  function translateHello() {
+    const languageCode = languageCodeInput.val();
+    $.get(`https://www.fourtonfish.com/hellosalut/hello/?lang=${languageCode}`, (data) => {
+      $('#hello').text(data.hello);
+    });
+  }
+});
