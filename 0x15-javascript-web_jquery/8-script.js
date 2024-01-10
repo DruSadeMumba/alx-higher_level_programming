172230a2813ed7efd08da32f11f5d5b0557915ef
Dru Mumba <drusademumba@gmail.com>
2024-01-10 12:54:12 +0300

    Write a JavaScript script that fetches and lists the title for all movies by using this URL: https://swapi-api.alx-tools.com/api/films/?format=jsonusing the JQuery API

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,6 @@
-#!/usr/bin/env node
+$.get("https://swapi-api.alx-tools.com/api/people/5/?format=json", (data) => {
+  const listMovies = $('#list_movies');
+  data.results.forEach((movie) => {
+    listMovies.append($('<li>').text(movie.title));
+  });
+});
